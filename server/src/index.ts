5ac346c19870a8681ec6b9f1f8b15b3b9c1a2352
Habib Hossain <habibfahim71@gmail.com>
2026-01-11 23:59:39 +0600

    endpoint set

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,14 @@ import express  from 'express';
  const app = express();
 
  app.get("/", (req, res)=> {
-    res.json({success: true});
+    res.json({
+        message: "Welcome to Productify API - Powered by PostgreSQL, Drizzle ORM & Clerk Auth"
+        endpoint: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        },
+    });
  });
 
  app.listen(3000, () => console.log('Server is up and running on PORT:3000'))
