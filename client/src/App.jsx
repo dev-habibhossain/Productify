2f05c7ba19fa0ec4c456b275b895e785aca29581
Habib Hossain <habibfahim71@gmail.com>
2026-01-20 01:43:07 +0600

    react router added some page path set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import ProfilePage from "./pages/ProfilePage";
+import CreatePage from "./pages/CreatePage";
+import EditProductPage from "./pages/EditProductPage";
 
 const App = () => {
     return (
-       <div>
-        <h1>Hello</h1>
-
-        <button className="btn btn-primary text-white">Click Me</button>
-       </div>
+      <div className="min-h-screen bg-base-100">
+        <Navbar />
+        <main className="max-w-5xl mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/edit/:id" element={<EditProductPage />} />
+          </Routes>
+        </main>
+      </div>
     );
 };
 
---
+++ client/src/pages/EditProductPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditProductPage = () => {
+    return (
+        <div>
+            Edit Product Page
+        </div>
+    );
+};
+
+export default EditProductPage;
