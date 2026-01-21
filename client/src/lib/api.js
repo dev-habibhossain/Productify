285e11812f2cebddb68b5721d5cd47d0a3aa4286
Habib Hossain <habibfahim71@gmail.com>
2026-01-21 23:56:09 +0500

    product crud done

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,38 @@
 import api from "./axios.js";
 
+// USER API
 export const syncUser = async (userData) => {
     const {data} =await api.post('/users/sync', userData);
     return data;
 };
+
+// PRODUCT API
+export const getAllProducts = async () => {
+    const {data} = await api.get('/products');
+    return data;
+};
+
+export const getProductById = async (id) => {
+    const {data} = await api.get(`/products/${id}`);
+    return data;
+};
+
+export const myProducts = async () => {
+    const {data} = await api.get('/products/my');
+    return data;
+};
+
+export const createProduct = async (productData) => {
+    const {data} = await api.post('/products', productData);
+    return data;
+};
+
+export const updateProduct = async ({id, ...productData}) => {
+    const { data } = await api.put(`/products/${id}`, productData);
+    return data;
+};
+
+export const deleteProduct = async (id) => {
+    const { data } = await api.delete(`/products/${id}`);
+    return data;
+}
