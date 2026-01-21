74af93b370db8a181f0df00f7620d61c77327cd8
Habib Hossain <habibfahim71@gmail.com>
2026-01-21 23:41:18 +0500

    tanstack and axios installed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
 import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from 'react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App";
+import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,12 +13,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
         <App />
+      </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
-)
+);
