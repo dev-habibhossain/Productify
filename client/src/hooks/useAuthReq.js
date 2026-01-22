e0bb3645e872cba725d2a32430e41a8bc8c22ebc
Habib Hossain <habibfahim71@gmail.com>
2026-01-22 23:50:12 +0200

    these hooks are checked

--- client/src/hooks/useAuthReq.js
+++ client/src/hooks/useAuthReq.js
@@ -1,10 +1,14 @@
 import { useAuth } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import api from "../lib/axios.js";
+
+let inInterceptorRegistered = false;
 function useAuthReq() {
     const { isSignedIn, getToken, isLoaded } = useAuth();
 
     useEffect(() => {
+        if (inInterceptorRegistered) return;
+        inInterceptorRegistered = true;
         const interceptor = api.interceptors.request.use(async (config) => {
             if (isSignedIn) {
                 const token = await getToken();
@@ -15,7 +19,10 @@ function useAuthReq() {
             return config;
         });
 
-        return () => api.interceptors.request.eject(interceptor);
+        return () => {
+            api.interceptors.request.eject(interceptor);
+            inInterceptorRegistered = false;
+        }
     }, [isSignedIn, getToken]);
 
     return { isSignedIn, isClerkLoaded: isLoaded };
