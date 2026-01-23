a03ad06ce9b1847206153272092aa23e70f6c2c7
Habib Hossain <habibfahim71@gmail.com>
2026-01-23 23:22:50 +0500

    home page loading, product fetch are done design remain

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useProducts } from "../hooks/useProducts";
+
 const HomePage = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+    const {data: products, isLoading, error} = useProducts();
+
+    if(isLoading) return <LoadingSpinner />;
+
+    if(error) {
+        return (
+            <div role="alert" className="alert alert-error">
+                <span>Error loading products: {error.message}.Please try again later.</span>
+            </div>
+        )
+    }
+
+    return <div>Home</div>
 };
 
 export default HomePage;
