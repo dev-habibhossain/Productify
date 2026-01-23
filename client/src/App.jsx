af7668ac6b5a5237589ac8282b8ac5afa203eb0a
Habib Hossain <habibfahim71@gmail.com>
2026-01-24 01:46:43 +0600

    product can add and its showing in home page as product card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
 import CreatePage from "./pages/CreatePage";
 import EditProductPage from "./pages/EditProductPage";
@@ -9,7 +9,7 @@ import useAuthReq from "./hooks/useAuthReq";
 import useUserSync from "./hooks/useUserSync";
 
 const App = () => {
-  const { isClerkLoaded } = useAuthReq();
+  const { isClerkLoaded, isSignedIn } = useAuthReq();
   useUserSync()
 
   if(!isClerkLoaded) return null;
@@ -22,7 +22,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/create" element={<CreatePage />} />
+          <Route path="/create" element={isSignedIn ?<CreatePage /> : <Navigate to='/' />} />
           <Route path="/edit/:id" element={<EditProductPage />} />
         </Routes>
       </main>
--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -1,6 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
-import { getAllProducts } from '../lib/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { createProduct, getAllProducts } from '../lib/api';
 export const useProducts = () => {
     const result = useQuery({queryKey:["products"],queryFn: getAllProducts});
     return result;
-}
+};
+
+export const useCreateProduct = () => {
+    return useMutation({mutationFn: createProduct})
+};
