b1e479ce7ff71edb5f82a62ce7023f8a636d7930
Habib Hossain <habibfahim71@gmail.com>
2026-01-24 01:09:58 +0600

    product card are created

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { PackageIcon, SparklesIcon } from "lucide-react";
 import { Link } from "react-router";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { useProducts } from "../hooks/useProducts";
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
   const { data: products, isLoading, error } = useProducts();
@@ -51,6 +52,25 @@ const HomePage = () => {
             <PackageIcon className="size-5 text-primary" />
             All Products
         </h2>
+
+        {products.length === 0 ? (
+            <div className="card bg-base-300">
+                <div className="card-body items-center text-center py-16">
+                    <PackageIcon className="size-16 text-base-content/20" />
+                    <h3 className="card-title text-base-content/50">No products found</h3>
+                    <p className="text-base-content/40 text-sm">Be the first to share something!</p>
+                    <Link to="/create" className="btn btn-primary btn-sm mt-2">
+                    Create Product
+                    </Link>
+                </div>
+            </div>
+        ):(
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        )}
       </div>
     </div>
   );
