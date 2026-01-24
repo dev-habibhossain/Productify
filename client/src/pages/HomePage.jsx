d0f0aa88bbe921e6df3e1578603e0c13f1d62d80
Habib Hossain <habibfahim71@gmail.com>
2026-01-24 23:58:09 +0600

    litle change

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { useProducts } from "../hooks/useProducts";
 import ProductCard from "../components/ProductCard";
+import { SignInButton } from "@clerk/clerk-react";
 
 const HomePage = () => {
   const { data: products, isLoading, error } = useProducts();
@@ -39,37 +40,43 @@ const HomePage = () => {
             <p className="py-4 text-base-content/60">
               Upload, Discover, and connect with creators.
             </p>
-            <Link to="/create" className="btn btn-primary">
-              <SparklesIcon className="size-4" />
-              Start Selling
-            </Link>
+            <SignInButton mode="modal">
+              <button className="btn btn-primary">
+                <SparklesIcon className="size-4" />
+                Start Selling
+              </button>
+            </SignInButton>
           </div>
         </div>
       </div>
       {/* Products */}
-      <div>
+      <div className="mt-10">
         <h2 className="text-xl font-bold flex items-center gap-2 mb-4">
-            <PackageIcon className="size-5 text-primary" />
-            All Products
+          <PackageIcon className="size-5 text-primary" />
+          All Products
         </h2>
 
         {products.length === 0 ? (
-            <div className="card bg-base-300">
-                <div className="card-body items-center text-center py-16">
-                    <PackageIcon className="size-16 text-base-content/20" />
-                    <h3 className="card-title text-base-content/50">No products found</h3>
-                    <p className="text-base-content/40 text-sm">Be the first to share something!</p>
-                    <Link to="/create" className="btn btn-primary btn-sm mt-2">
-                    Create Product
-                    </Link>
-                </div>
+          <div className="card bg-base-300">
+            <div className="card-body items-center text-center py-16">
+              <PackageIcon className="size-16 text-base-content/20" />
+              <h3 className="card-title text-base-content/50">
+                No products found
+              </h3>
+              <p className="text-base-content/40 text-sm">
+                Be the first to share something!
+              </p>
+              <Link to="/create" className="btn btn-primary btn-sm mt-2">
+                Create Product
+              </Link>
             </div>
-        ):(
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         )}
       </div>
     </div>
